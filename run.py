13417f26c6813648fa07fa6c25b85f1431d84926
juki <jukijukirthan12@gmail.com>
2026-06-09 06:14:26 +0530

    run.py add

---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+from app import create_app
+
+app = create_app()
+CORS(app)
+
+if __name__ == "__main__":
+    with app.app_context():
+        from app.extensions import db
+        db.create_all()
+    app.run(debug=True, port=5000)
